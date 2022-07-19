946bc4c3d2d37b87f0882673518007a9ded548c1
Ricardo Delgado <ricardo.delgado@penn-interactive.com>
2022-07-19 09:03:59 -0400

    fix fragment

--- src/components/SbEventRow.js
+++ src/components/SbEventRow.js
@@ -2,17 +2,15 @@ import '../styles/EventRow.scoped.scss'
 
 const SbEventRow = ({ children, name }) => {
   return (
-    <>
-      <div className="events">
-        <h3>{name}</h3>
+    <div className="events">
+      <h3>{name}</h3>
 
-        <div className="events-row">
-          {children.map((child, index) => (
-            <div key={index}>{child}</div>
-          ))}
-        </div>
+      <div className="events-row">
+        {children.map((child, index) => (
+          <div key={index}>{child}</div>
+        ))}
       </div>
-    </>
+    </div>
   )
 }
 
